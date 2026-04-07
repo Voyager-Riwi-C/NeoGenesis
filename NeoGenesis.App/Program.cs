38b936127743294c23a03ed7d82873b3895163a2
daniquinto <dany.quintorios@gmail.com>
2026-04-07 13:23:14 -0500

    chore: base architecture setup

---
+++ NeoGenesis.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using NeoGenesis.App.Data;
+using NeoGenesis.App.Repositories;
+using NeoGenesis.App.Services;
+using NeoGenesis.App.UI;
+using Microsoft.EntityFrameworkCore;
+
+var db = new AppDbContext();
+db.Database.Migrate();
+
+var repo = new DinosaurRepository(db);
+var service = new DinosaurService(repo);
+var menu = new MainMenu(service);
+
+menu.Show();
